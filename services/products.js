365b3cb092e745d5718c44f942657e26517ba2d4
hheloisa <hhackenhaar@gmail.com>
2021-10-19 18:32:18 -0300

    Feat: getAll products service

--- services/products.js
+++ services/products.js
@@ -1,6 +1,11 @@
 const productsModel = require('../models/products');
 const { codes, errors } = require('../messages');
 
+async function getAll() {
+  const allProducts = await productsModel.getAll();
+  return { products: allProducts };
+}
+
 async function insertProduct(name, quantity) {
   const checkName = await productsModel.findByName(name);
     if (checkName) {
@@ -11,4 +16,4 @@ async function insertProduct(name, quantity) {
   return { newProductService };
 }
 
-module.exports = { insertProduct };
+module.exports = { getAll, insertProduct };
