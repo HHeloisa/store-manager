02ce3bd6f7f4b0f9ce35d485b8a5199d78f90988
hheloisa <hhackenhaar@gmail.com>
2021-10-19 16:21:12 -0300

    Compoem o arquivo index, definindo os recursos

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const productsRoutes = require('./routes/productsRoutes');
+const productsIdRoutes = require('./routes/productsIdRoutes');
+const salesRoutes = require('./routes/salesRoutes');
+const salesIdRoutes = require('./routes/salesIdRoutes');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.use('/products', productsRoutes);
+app.use('/products/:id', productsIdRoutes);
+app.use('/sales', salesRoutes);
+app.use('/sales/:id', salesIdRoutes);
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log('Online');
 });
