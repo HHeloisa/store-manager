6422965ad91a3e36343eac82a2c1731afb80289b
hheloisa <hhackenhaar@gmail.com>
2021-10-19 19:16:07 -0300

    Libera rota /get/:id sales

--- routes/salesRoutes.js
+++ routes/salesRoutes.js
@@ -4,8 +4,8 @@ const { validateSales } = require('../validations/validations');
 
 router.post('/', validateSales, salesController.insertSale);
 router.get('/', salesController.getAllSales);
-/* router.get('/:id', salesController.findSaleById);
-router.put('/:id', validateSales, salesController.updateSale);
+router.get('/:id', salesController.findSaleById);
+/* router.put('/:id', validateSales, salesController.updateSale);
 router.delete('/:id', salesController.deleteSale); */
 
 module.exports = router;
