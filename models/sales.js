3084b20279f0f205229bc95fef490cfa3ba848e1
hheloisa <hhackenhaar@gmail.com>
2021-10-19 19:14:30 -0300

    Feat: getById sales model

--- models/sales.js
+++ models/sales.js
@@ -1,9 +1,18 @@
+const { ObjectId } = require('mongodb');
 const connection = require('./connection');
 
 const getAllSales = async () => {
   const db = await connection();
   const allSales = await db.collection('sales').find().toArray();
   return allSales;
+};
+
+const findSaleById = async (id) => {
+  if (!ObjectId.isValid(id)) return null;
+
+  const db = await connection();
+  const findOne = await db.collection('sales').findOne({ _id: ObjectId(id) });
+  return findOne;
 };
 
 const insertSale = async (itensSold) => {
@@ -13,4 +22,4 @@ const insertSale = async (itensSold) => {
   return { _id: sale.insertedId, itensSold };
 };
 
-module.exports = { getAllSales, insertSale };
+module.exports = { getAllSales, findSaleById, insertSale };
