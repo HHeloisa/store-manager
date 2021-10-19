20d9bb03a79367dfa5a31481917f013ebf152e3c
hheloisa <hhackenhaar@gmail.com>
2021-10-19 19:05:31 -0300

    Feat: create sales model

---
+++ models/sales.js
@@ -0,0 +1,10 @@
+const connection = require('./connection');
+
+const insertSale = async (itensSold) => {
+  const db = await connection();
+  const sale = await db.collection('sales').insertOne({ itensSold });
+  
+  return { _id: sale.insertedId, itensSold };
+};
+
+module.exports = { insertSale };
