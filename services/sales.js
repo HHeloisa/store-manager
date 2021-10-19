86a71503289a709b29ec342b1cac6c1a6dcb9720
hheloisa <hhackenhaar@gmail.com>
2021-10-19 19:15:13 -0300

    Feat: getById sales service

--- services/sales.js
+++ services/sales.js
@@ -1,8 +1,17 @@
 const salesModels = require('../models/sales');
+const { codes, errors } = require('../messages');
 
 async function getAllSales() {
   const allSales = await salesModels.getAllSales();
   return { sales: allSales };
+}
+
+async function findSaleById(id) {
+  const findSale = await salesModels.findSaleById(id);
+  if (!findSale) { 
+    return { err: { code: codes.notFound, message: errors.saleNotFound } };
+  }
+  return { findSale };
 }
 
 async function insertSale(itensSold) {
@@ -10,4 +19,4 @@ async function insertSale(itensSold) {
   return { insertedSale };
 }
 
-module.exports = { getAllSales, insertSale };
+module.exports = { getAllSales, findSaleById, insertSale };
