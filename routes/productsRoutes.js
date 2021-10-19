72ca595964df066e7a190f2360e959f74dc9d91e
hheloisa <hhackenhaar@gmail.com>
2021-10-19 18:33:24 -0300

    Libera rota de getAll

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const productsController = require('../controllers/products');
 const { validateProducts } = require('../validations/validations');
 
-// router.get('/', productsController.getAll);
+router.get('/', productsController.getAll);
 router.post('/', validateProducts, productsController.insertProduct);
 
 module.exports = router;
