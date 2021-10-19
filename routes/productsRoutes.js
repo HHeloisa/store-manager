8c800b402e44319b660763ac776601ba77bb2170
hheloisa <hhackenhaar@gmail.com>
2021-10-19 18:58:12 -0300

    Libera rota de /update products

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -5,7 +5,7 @@ const { validateProducts } = require('../validations/validations');
 router.get('/', productsController.getAll);
 router.get('/:id', productsController.findById);
 router.post('/', validateProducts, productsController.insertProduct);
-/* router.put('/', validateProducts, productsController.updateProducts);
-router.delete('/', productsController.deleteProduct); */
+router.put('/', validateProducts, productsController.updateProducts);
+/* router.delete('/', productsController.deleteProduct); */
 
 module.exports = router;
