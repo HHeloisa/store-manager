21f59400105ccc10b4b71f5ffd9c0d1cc77842ea
hheloisa <hhackenhaar@gmail.com>
2021-10-19 19:11:51 -0300

    Feat: getAll sales service

--- services/sales.js
+++ services/sales.js
@@ -1,8 +1,13 @@
 const salesModels = require('../models/sales');
 
+async function getAllSales() {
+  const allSales = await salesModels.getAllSales();
+  return { sales: allSales };
+}
+
 async function insertSale(itensSold) {
   const insertedSale = await salesModels.insertSale(itensSold);
   return { insertedSale };
 }
 
-module.exports = { insertSale };
+module.exports = { getAllSales, insertSale };
